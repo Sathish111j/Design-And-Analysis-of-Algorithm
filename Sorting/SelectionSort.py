ced7ff7947a75714424528c1d636a2fae750adfb
Sathish111j <sathish111j@gmail.com>
2023-11-25 09:16:46 +0530

    insertion  sort

---
+++ Sorting/SelectionSort.py
@@ -0,0 +1,25 @@
+arr = [5,4,3,2,1]
+
+
+def SelectionSort(arr):
+    for i in range(len(arr)):
+        maxindex = 0
+        for j in range(len(arr) - i):
+            if arr[j] > arr[maxindex]:
+                maxindex = j
+        arr[maxindex], arr[len(arr) - i-1] = arr[len(arr) - i-1], arr[maxindex]
+
+
+SelectionSort(arr)
+
+for k in arr:
+    print(k, end=" ")
+
+'''
+Time Complexity	 
+Best	O(n2)
+Worst	O(n2)
+Average	O(n2)
+Space Complexity	O(1)
+Stability	No
+'''
